92d03fcd1e6dba39edcabfc7b03967cc4012620a
SurajK-555 <kumarsuraj59080@gmail.com>
2025-01-08 01:01:42 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import logo from '../assets/KevinRushLogo.png'
+import logo from '../assets/kevinRushLogo.png'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 import { FaInstagram} from 'react-icons/fa'
@@ -8,9 +8,9 @@ import { FaInstagram} from 'react-icons/fa'
 const Navbar = () => {
   return (
     <nav className=' mb-20 flex items-center justify-between py-6'>
-    {/* <div className=' flex flex-shrink-0 items-center'>
+    <div className=' flex flex-shrink-0 items-center'>
       <img className='mx-2 w-10' src={logo} alt="logo" />
-    </div> */}
+    </div>
     <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
         <FaLinkedin/>
         <FaGithub/>
